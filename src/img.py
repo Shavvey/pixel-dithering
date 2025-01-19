b608b5a8c03251b2da913fb328524b0963423d3d
Cole Johnson <coledjohn@gmail.com>
2025-01-18 17:17:42 -0700

    adding some comments and headers to `Image` methods

--- src/img.py
+++ src/img.py
@@ -6,12 +6,20 @@ class Image:
     image: pl.Image  # loaded image to use dithering
 
     def __init__(self, path: str):
+        """Creates the initial `Image` object given a relative path"""
         self.path = path
         self.image = pl.open(path)
 
-    def ropen(self):
-        pl.open("r", self.path)
+    def wopen(self) -> pl.Image:
+        """Returns a writable image, which will be used in
+        the dithering algorithm"""
+        return pl.open("w", self.path)
 
     def dither(self) -> pl.Image:
+        """Modify the image to produce the final dithering result"""
         i = pl.new("RGB", (200, 200), 1)
         return i
+
+    def show(self):
+        """Simple method to show resulting image"""
+        self.image.show()
