f67872d4df6c0ce1f229fe9762c7f73459a8eaab
Cole Johnson <coledjohn@gmail.com>
2025-02-20 09:32:17 -0700

    trying to use new `Matrix` class to compute Bayer dither level

--- src/dither/bayer.py
+++ src/dither/bayer.py
@@ -1,18 +1,19 @@
 from img import Image
+from matrix import Matrix
 
-type Matrix = list[list[int]]  # Nx2 matrix type
 
 def mult_plus_sum(a: int, b: int, n: int) -> int:
-    """ A recursive sum of an expression of the form a*n + b,
+    """A recursive sum of an expression of the form a*n + b,
     where `a` is the multiplication term, and `b` is the plus term"""
     if n == 0:
-       return 0
+        return 0
     else:
-        return (a * mult_plus_sum(a, b, n - 1) + b)
-    
+        return a * mult_plus_sum(a, b, n - 1) + b
+
 
 class Bayer:
-    matrix: Matrix = [[0, 0], [0, 0]]
+    matrix: Matrix = Matrix([[0, 0], [0, 0]])
+
     def __init__(self, level: int) -> None:
         """Create a Bayer matrix which an associated level"""
         self.matrix = self.make_bayer(level)
@@ -26,13 +27,18 @@ class Bayer:
         a01 = mult_plus_sum(4, 2, level)
         a10 = mult_plus_sum(4, 3, level)
         a11 = mult_plus_sum(4, 1, level)
-        return [[a00, a01],
-                [a10, a11]]
+        return Matrix([[a00, a01], [a10, a11]])
+
 
 def bayer(bayer: Bayer, img: Image) -> Image:
     """Dithering algorithm that uses a Bayer matrix
 
-    param `bayer`: contructed Bayer matrix, based on different 'levels' 
+    param `bayer`: contructed Bayer matrix, based on different 'levels'
     param `img`: underlying image that we will used for bayer dithering
     """
+    # return the closest color, compare brightness levels
+    for b in bayer.matrix:
+        # iter across image, use elements inside bayer matrix to dither??
+        for pixel in img:
+            pass
     return img
